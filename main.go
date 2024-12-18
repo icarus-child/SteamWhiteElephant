86971913275bb650626aa214eeb182b55ee3a55c
Ethan Meier <e652@hotmail.ca>
2024-12-17 18:16:00 -0800

    tls added but it breaks websockets

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"main/router"
 
-	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := router.InitRouter()
+	r.SetTrustedProxies(nil)
 	// r.Run(":80")
-	autotls.Run(r, "ethanmeier.com")
+	r.RunTLS(":443", "domain.cert.pem", "private.key.pem")
 }
